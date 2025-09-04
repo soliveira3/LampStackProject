66838a24378e1d53c8bc0570a08d3c301d9b38ed
Ori <orioh15@gmail.com>
2025-09-04 13:34:57 -0400

    Added LAMP API folder for backend

---
+++ LAMPAPI/cors.php
@@ -0,0 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { exit; }
+
+header("Content-Type: application/json");
